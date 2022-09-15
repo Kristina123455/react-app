66e9f4f4a0b25b7cbcf19052f718a284fd6ec947
Kristina123455 <kristinaneskovic9@gmail.com>
2022-09-15 17:37:32 +0200

    listing saved books option

--- src/components/SavedBooks.jsx
+++ src/components/SavedBooks.jsx
@@ -1,7 +1,12 @@
-const SavedBooks = () => {
+import Book from './Book'
+
+const SavedBooks = ({books}) => {
   return (
-    <div>
+    <div className='cart-container'>
         <h3>Saved books</h3>
+        {books.map((b)=> (
+         <Book book = {b} key={b.id} saved={0}/>
+       ))}
     </div>
   )
 }
