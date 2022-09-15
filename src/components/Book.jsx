62abc950cf180478ebc897a9edd66f26030f3d79
Kristina123455 <kristinaneskovic9@gmail.com>
2022-09-15 17:38:27 +0200

    classname changable

--- src/components/Book.jsx
+++ src/components/Book.jsx
@@ -1,21 +1,30 @@
-import React from 'react';
-import {BsBookmarkFill,BsInfoSquareFill} from 'react-icons/bs';
+import React from "react";
+import { BsBookmarkFill, BsInfoSquareFill } from "react-icons/bs";
 
-function Book({book, onAdd}) {
-
-//    function onAdd(title){    }
+function Book({ book, onAdd, saved }) {
+  //    function onAdd(title){    }
 
   return (
-    <div className='book-card'>
-        <img className='book-img' src="https:/picsum.photos/200" alt="book-img" />
-        <div className="book-card-body">
-            <h3 className="book-card-title">{book.title}</h3>
-            <p className="book-desc">{book.description}</p>
-        </div>
-        <button className="btn" onClick={()=>onAdd(book.title)}><BsBookmarkFill /></button>
-        <button className="btn"><BsInfoSquareFill /></button>
+    <div className={saved===1 ? "book-card" : "card-cart"}>
+      <img className={saved===1 ? "card-img-top" : "card-img-left"} src="https:/picsum.photos/200" alt="book-img" />
+      <div className="book-card-body">
+        <h3 className="book-card-title">{book.title}</h3>
+        <p className="book-desc">{book.description}</p>
+      </div>
+      {saved === 1 ? (
+        <>
+          <button className="btn" onClick={() => onAdd(book.title)}>
+            <BsBookmarkFill />
+          </button>
+          <button className="btn">
+            <BsInfoSquareFill />
+          </button>
+        </>
+      ) : (
+        <></>
+      )}
     </div>
-  )
+  );
 }
 
-export default Book
+export default Book;
