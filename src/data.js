e5c6dc2508a31a006fc5ff18107c84b5aa746c2b
Kristina123455 <kristinaneskovic9@gmail.com>
2022-09-21 10:48:05 +0200

    Cards array initialized

---
+++ src/data.js
@@ -0,0 +1,58 @@
+export const postInfo = [
+    {
+        id: 1,
+        headerTitle: 'Najbolje prodavnice',
+        headerSubtitle: 'Srbija',
+        headerText: 'Akcije traju do 15. novembra',
+        title: 'Prodavnice',
+        description: 'Sajtovi prodavnica',
+        img: 'https://cdn.pixabay.com/photo/2016/04/01/00/21/animals-1298132__340.png'
+    },
+    {
+        id: 2,
+        headerTitle: 'Raspored filmova',
+        headerSubtitle: 'U svim bioscima',
+        headerText: 'Popust 15% na kartama.',
+        title: 'Raspored filmova',
+        description: 'Lista bioskopa sirom Srbije',
+        img: 'https://cdn.pixabay.com/photo/2016/06/14/10/42/scarecrow-1456235__340.png'
+    },
+    {
+        id: 3,
+        headerTitle: 'Igraonica & Rodjendaonica',
+        headerSubtitle: 'Rezervisite mesto na vreme',
+        headerText: 'Radno vreme: 10-21h',
+        title: 'Igraonica/Rodjendaonica',
+        description: 'Opis aktivnosti dece.',
+        img: 'https://cdn.pixabay.com/photo/2016/04/01/00/13/halloween-1298060__340.png'
+    },
+    {
+        id: 4,
+        headerTitle: 'HALLOWEEN QUIZ TIME',
+        headerSubtitle: 'Kviz poznavanja Halloween-a',
+        headerText: 'Budi deo HALLOQUIZ-a',
+        title: 'Raspored kvizova',
+        description: 'Raspored kvizova po satima',
+        img: 'https://cdn.pixabay.com/photo/2014/04/03/10/55/cobweb-311734__340.png'
+    },
+    {
+        id: 5,
+        headerTitle: 'TALENT KIDS SHOW',
+        headerSubtitle: 'Takmicenje za decu uzrasta od 4 do 12 godina',
+        headerText: 'Uclani se i ti!',
+        title: 'Raspored takmicenja za decu',
+        description: 'Takmicenja za decu od 4 do 12 godina.',
+        img: ' https://cdn.pixabay.com/photo/2016/05/30/13/49/witch-1424756__340.png'
+       
+    },
+    ,
+    {
+        id: 7,
+        headerTitle: 'Raspored predstava',
+        headerSubtitle: 'Predstave za decu',
+        headerText: 'Raspored za naredni vikend',
+        title: 'Raspored predstava',
+        description: 'Raspored predstava za naredni vikend',
+        img: 'https://cdn.pixabay.com/photo/2016/04/01/00/23/amphibian-1298147__340.png'
+    }
+];
