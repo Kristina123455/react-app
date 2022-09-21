f041a6744dbcb64b683bf69fcdcbebfddc32c962
Kristina123455 <kristinaneskovic9@gmail.com>
2022-09-21 10:49:18 +0200

    navigration bar component code

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,21 +1,35 @@
 import React from 'react';
-import {ImBooks} from 'react-icons/im';
-import {Link} from 'react-router-dom';
+import logo from '../duh.png';
+import { Link } from 'react-router-dom';
 
-function NavBar({booksText}) {
+function Navbar(){
+    return(
+    <nav className="navbar navbar-expand-lg navbar-light bg-light" >
     
-    return (
-        <div className='navBar'>
-            <Link to="/">Books</Link>
-            <Link to="/savedBooks" className="cart-items">
-                <ImBooks/>Saved books
-                <p className="navBar-books-text">{booksText}</p>
-            </Link>
-
-        </div>
-
-    )
-
-}
-
-export default NavBar
+    <div className="container-fluid">
+    <Link className="navbar-brand ml-5" to="/">
+        <img src={logo} alt="logo" style={{width:'90px'}}></img>
+    </Link>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+      <span>
+        <i className="fas fa-bars" style= {{ color: '#000' }}></i>
+      </span>
+    </button>
+    <div className="collapse navbar-collapse" id="navbarNavDropdown">
+      <ul className="navbar-nav m-auto">
+        <li className="nav-item active ">
+          <Link className="nav-link active text-dark text-uppercase ml-5" aria-current="page" to="/">Početna</Link>
+        </li>
+        <li className="nav-item">
+          <Link className="nav-link text-dark text-uppercase  ml-5" to="/kontakt">Kontakt</Link>
+        </li>
+        <li className="nav-item">
+          <Link className="nav-link text-dark text-uppercase ml-5" to="/onama">O nama</Link>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+);
+};
+export default Navbar;
